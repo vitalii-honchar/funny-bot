4a98dfc8dd317924e7bd20cbc51b68af846a3a23
Vitalii Honchar <weaxme@gmail.com>
2022-12-21 07:20:43 -0500

    Added docker-compose.yaml

---
+++ env/postgres/init.sql
@@ -0,0 +1,8 @@
+create table tg_user (
+    "id" serial primary key,
+    "first_name" varchar(100) not null,
+    "last_name" varchar(100) not null,
+    "username" varchar(100) not null,
+    "chat_id" bigint not null,
+    "notification_time" timestamp not null
+);
